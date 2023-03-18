0e23ce8847ff800e1495842ec602cd746b56fc74
Tushar Upadhyay <tusharupadhyay691@gmail.com>
2023-03-18 22:30:47 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import BackTop from "./components/BackTop";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Home />
+      <Footer />
+      <BackTop />
+    </>
+  );
+}
+
+export default App;
